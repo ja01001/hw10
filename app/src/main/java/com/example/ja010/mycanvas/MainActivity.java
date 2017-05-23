ea81e30dc879bb8afe66fddffdff9f6078a80ac6
ja01001 <ja01001@naver.com>
2017-05-24 00:12:48 +0900

    filter clear but, not default and store and load

--- app/src/main/java/com/example/ja010/mycanvas/MainActivity.java
+++ app/src/main/java/com/example/ja010/mycanvas/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ja010.mycanvas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,27 +21,65 @@ public class MainActivity extends AppCompatActivity {
         cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                mc.ch(b);
+                mc.ch(b);// 눌렸을때의 값을 전달
             }
         });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(0,1,0,"Bluring");
-        menu.add(0,2,0,"Coloring");
-        menu.add(0,3,0,"Pen Width Big");
+        getMenuInflater().inflate(R.menu.menu,menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.blur:
+                if(item.isChecked()){
+                    item.setChecked(false);
+                    mc.blur(false,cb.isChecked());
+                }
+                else{
+                    item.setChecked(true);
+                    mc.blur(true,cb.isChecked());
+                }
+                break;
+            case R.id.col:
+                if(item.isChecked()){
+                    item.setChecked(false);
+                    mc.colors(false,cb.isChecked());
+                }
+                else{
+                    item.setChecked(true);
+                    mc.colors(true,cb.isChecked());
+                }
+                break;
+            case R.id.pwb:
+                if(item.isChecked()){
+                    item.setChecked(false);
+                    mc.pwb(false);
+                }
+                else{
+                    item.setChecked(true);
+                    mc.pwb(true);
+
+                }
+                break;
+            case R.id.red:
+                mc.red();
+                break;
+            case R.id.blue:
+                mc.blue();
+                break;
+        }
         return super.onOptionsItemSelected(item);
     }
+
     public void clcl(View v){
         switch (v.getId()){
             case R.id.b1:
-
+                mc.clear();
                 break;
             case R.id.b2:
                 break;
@@ -48,13 +87,20 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.b4:
                 mc.rotate(cb.isChecked());
+                cb.setChecked(true);
                 break;
             case R.id.b5:
                 mc.move(cb.isChecked());
+                cb.setChecked(true);
                 break;
             case R.id.b6:
+
+                mc.sizeup();
+                cb.setChecked(true);
                 break;
             case R.id.b7:
+                mc.skew();
+                cb.setChecked(true);
                 break;
         }
     }
--- app/src/main/java/com/example/ja010/mycanvas/mycanvas.java
+++ app/src/main/java/com/example/ja010/mycanvas/mycanvas.java
@@ -3,10 +3,15 @@ package com.example.ja010.mycanvas;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -20,20 +25,21 @@ public class mycanvas extends View {
     Canvas mCanvas;
     int oldx = -1 ;
     int oldy = -1;
-    boolean checked;
+    boolean checked = false;
     Paint mpaint= new Paint();
     Bitmap img = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
+    Bitmap img1 = img;
     public mycanvas(Context context) {
         super(context);
         mpaint.setColor(Color.BLACK);
-        mpaint.setStrokeWidth(5);
+        mpaint.setStrokeWidth(3);
         this.setLayerType(LAYER_TYPE_SOFTWARE,null);
     }
 
     public mycanvas(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         mpaint.setColor(Color.BLACK);
-        mpaint.setStrokeWidth(5);
+        mpaint.setStrokeWidth(3);
         this.setLayerType(LAYER_TYPE_SOFTWARE,null);
     }
     private void drawstamp(int x, int y){
@@ -46,6 +52,7 @@ public class mycanvas extends View {
         if(mBitmap != null) {
             canvas.drawBitmap(mBitmap, 0, 0, null);
         }
+
 
     }
     @Override
@@ -60,24 +67,33 @@ public class mycanvas extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        int x = (int) event.getX();
-        int y = (int) event.getY();
+        int x =-1;
+        int y = -1;
 
+        x = (int) event.getX();
+        y = (int) event.getY();
             if (event.getAction() == MotionEvent.ACTION_DOWN) {
                 oldx = x;
                 oldy = y;
             } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
                 if (oldx != -1) {
-                    if(!checked)
-                    mCanvas.drawLine(oldx, oldy, x, y, mpaint);
+                    if(!checked) {
+                        mCanvas.drawLine(oldx, oldy, x, y, mpaint);
+                    }
                     invalidate();
                     oldx = x;
                     oldy = y;
                 }
             } else if (event.getAction() == MotionEvent.ACTION_UP) {
                 if (oldx != -1) {
+                    if(checked){
+                        drawstamp(oldx,oldy);
+                    }
+                    else{
                     mCanvas.drawLine(oldx, oldy, x, y, mpaint);
+                    }
                     invalidate();
+
                 }
                 oldx = -1;
                 oldy = -1;
@@ -86,17 +102,94 @@ public class mycanvas extends View {
     }
     public void rotate(boolean checked){
         if(checked){
-            mCanvas.rotate(30,this.getWidth()/2,this.getHeight()/2);
-            invalidate();
+            mCanvas.rotate(30,mCanvas.getWidth()/2,mCanvas.getHeight()/2);
         }
+        else{ // rotate 한 값들을 다시 바꿔줘야하는데 이를 어쩌나 ...
+            img = img1;
+        }
+
     }
     public void move(boolean checked){
         if(checked) {
             mCanvas.translate(10, 10);
-            invalidate();
+        }
+        else { // move 한 값 변경
+            img = img1;
+        }
+
+    }
+    public void sizeup(){
+        if(checked){
+            img = Bitmap.createScaledBitmap(img,(int)(img.getWidth()*1.5),(int)(img.getHeight()*1.5),false);
+
+        }
+        else if(!checked){
+           img = img1;
         }
     }
     public void ch(boolean check){
         checked = check;
+        mCanvas.rotate(0,mCanvas.getWidth()/2,mCanvas.getHeight()/2);
+    }
+    public void clear(){// eraser 버튼
+        mCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
+        invalidate();
+    }
+    public void skew(){
+        if(checked){
+            mCanvas.skew(0.2f,0.2f);
+        }
+        else{
+            img = img1;
+        }
+    }
+    public void red(){
+        mpaint.setColor(Color.RED);
+    }
+    public void blue(){
+        mpaint.setColor(Color.BLUE);
+    }
+    public void pwb(boolean ch){
+        if(ch){
+            mpaint.setStrokeWidth(5);
+        }
+        else {
+            mpaint.setStrokeWidth(3);
+        }
+
+    }
+    public void colors(boolean mcheck, boolean stampch){
+        if(stampch==true&&mcheck==true){
+            float arr[] = {
+                    2.0f,0,0,0,-2.5f,
+                    0,2.0f,0,0,-2.5f,
+                    0,0,2.0f,0,-2.5f,
+                    0,0,0,1.0f,0
+            };
+            ColorMatrix mat = new ColorMatrix(arr);
+            ColorMatrixColorFilter filter = new ColorMatrixColorFilter(mat);
+            mpaint.setColorFilter(filter);
+        }
+        else{
+            float arr[] = {
+                    1.0f,0,0,0,2.5f,
+                    0,1.0f,0,0,2.5f,
+                    0,0,1.0f,0,2.5f,
+                    0,0,0,1.0f,0
+            };
+            ColorMatrix mat = new ColorMatrix(arr);
+            ColorMatrixColorFilter filter = new ColorMatrixColorFilter(mat);
+            mpaint.setColorFilter(filter);
+        }
+
+    }
+    public void blur(boolean mcheck, boolean stampch){
+        if(mcheck==true&&stampch==true){
+            BlurMaskFilter blur = new BlurMaskFilter(100, BlurMaskFilter.Blur.INNER);
+            mpaint.setMaskFilter(blur);
+        }
+        else{
+            mpaint.setMaskFilter(null);
+        }
     }
 }
